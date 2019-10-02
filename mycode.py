b65719bea979c81f6c01c5c48a3c8ea0d0b3aebe
pop <surachet.suw@gmail.com>
2019-10-02 10:20:55 +0700

    [FiX] Add sum function

--- mycode.py
+++ mycode.py
@@ -4,4 +4,12 @@ import datetime
 if a == 1:
     a = 2
     print datetime.datetime.now()
+
+def sum(a, b):
+    """docstring for test_int"""
+    if a and b:
+        return a + b
+
+res = sum(15, 35)
+print res
 #end
