d54c1ebf73535e842f9cf8331b5ed6d8890f2aab
pop <surachet.suw@gmail.com>
2019-10-02 10:24:30 +0700

    [FiX] Add some minus function

--- yourcode.py
+++ yourcode.py
@@ -1,7 +1,16 @@
 #This is Your Code
 #if you wanna use this code please contact me
+import mycode
 import datetime
 if a == 1:
     a = 2
     print datetime.datetime.now()
+def minus(a, b):
+    """docstring for minus"""
+    if a and b:
+        return a - b
+
+res1 = mycode.sum(100, 200)
+res2 = minus(res1, 70)
+print res2
 #end
